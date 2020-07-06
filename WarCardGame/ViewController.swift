26f247c9ba84bd0378d6df60969e376dce8c064e
wenogk <drf325@nyu.edu>
2020-07-06 16:45:20 +0400

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Romeno Wenogk Fernando on 06/07/2020.
+//  Copyright © 2020 Romeno Wenogk Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
